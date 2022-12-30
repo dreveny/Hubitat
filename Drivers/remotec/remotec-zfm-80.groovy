d4546e5e2a70e0352f291c5f15c674d650c41d4f
dreveny <dreveny@gmail.com>
2022-12-29 23:32:43 -0800

    Minor clean up with organization and logging.

--- Drivers/remotec/remotec-zfm-80.groovy
+++ Drivers/remotec/remotec-zfm-80.groovy
@@ -17,58 +17,89 @@
  *   2015-02-21 fixed null error on initial install
  *   2018-05-22 Forked to clean up and add new options and added momentary switch option.
  *   2022-09-01 Modified to work with Hubitat
+ *   2022-12-25 Minor clean up and make it look more like a Hubitat driver's logging.
 */
 
 metadata {
 
   definition (name: "Remotec ZFM-80", namespace: "dreveny", author: "Chad Dreveny") {
     capability "Actuator"
-    capability "Switch"
+    capability "Initialize"
+    capability "Momentary"
     capability "Polling"
     capability "Refresh"
-    capability "Momentary"
-    capability "Sensor"
     capability "Relay Switch"
-
+    capability "Sensor"
+    capability "Switch"
+ 
     fingerprint deviceId: "0x1003", inClusters: "0x20, 0x25, 0x27, 0x72, 0x86, 0x70, 0x85"
   }
   preferences {
-    input name: "param1", type: "enum", title: "Set external switch mode:",
-        description: "Switch type", required: true,
-        options: ["Disabled", "Momentary NO", "Momentary NC", "Toggle NO",
-                  "Toggle NC"]
-    input name: "param2", type: "enum", title: "Auto shutoff minutes:",
-        description: "Minutes?", required: false,
-        options: ["Never", "1", "5", "30", "60", "90", "120", "240"]
+    input name: "extSwitchType", type: "enum", title: "Set external switch mode:",
+      description: "Switch type", defaultValue: "Toggle NO", required: true,
+      options: ["Disabled", "Momentary NO", "Momentary NC", "Toggle NO", "Toggle NC"]
+    input name: "autoShutoffMinutes", type: "enum", title: "Auto shutoff minutes:",
+      description: "Minutes?", required: false,
+      options: ["Never", "1", "5", "30", "60", "90", "120", "240"]
     input name: "momentary", type: "number", title: "Momentary relay seconds (0 to disable):",
-        description: "Seconds after which relay will be closed"
-        required: false
+      description: "Seconds after which relay will be closed"
+      required: false
+    input name: "txtEnable", type: "bool", title: "Enable descriptionText logging", defaultValue: true
+    input name: "debugEnable", type: "bool", title: "Enable debug logging", defaultValue: false
   }
 }
 
-def installed() {
-  zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
+/****************************
+ * Set up
+****************************/
+void installed() {
+	logDebug "installed..."
+	initialize()
 }
 
+void initialize() {
+	logDebug "initialize"
+	refresh()
+}
+
+def configure() {
+	logDebug "configure"
+  // Configure the device to the defaults (Toggle Normally Open switch, no auto-shutoff).
+  delayBetween([
+      zwave.configurationV1.configurationSet(parameterNumber: 1, size: 1, configurationValue: [3]).format(),
+      zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, configurationValue: [0]).format(),
+      zwave.configurationV1.configurationSet(parameterNumber: 3, size: 1, configurationValue: [0]).format(),
+      zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1, configurationValue: [0]).format()
+  ])
+}
+
+/****************************
+ * Core functionality
+****************************/
+
 def parse(String description) {
+  logDebug "parse(): $description"
   def result = null
   def cmd = zwave.parse(description, [0x20: 1, 0x70: 1])
   if (cmd) {
     result = createEvent(zwaveEvent(cmd))
+    if (result != null && !result.empty) logInfo(result.toString())
   }
-  log.debug "Parse returned ${result?.descriptionText}"
   return result
 }
 
 def zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd) {
+  logDebug "BasicReport: $cmd"
   [name: "switch", value: cmd.value ? "on" : "off", type: "physical"]
 }
 
 def zwaveEvent(hubitat.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
+  logDebug "SwitchBinaryReport: $cmd"
   [name: "switch", value: cmd.value ? "on" : "off", type: "digital"]
 }
 
 def zwaveEvent(hubitat.zwave.commands.hailv1.Hail cmd) {
+  logDebug "Hail: $cmd"
   [name: "hail", value: "hail", descriptionText: "Switch button was pressed", displayed: false]
 }
 
@@ -92,11 +123,11 @@ def zwaveEvent(hubitat.zwave.commands.manufacturerspecificv2.ManufacturerSpecifi
   }
 
   if (productName) {
-    log.debug "Relay found: $productName"
+    logDebug "Relay found: $productName"
     updateDataValue("productName", productName)
   }
   else {
-    log.debug "Not a relay, retyping to Z-Wave Switch"
+    logDebug "Not a relay, retyping to Z-Wave Switch"
     setDeviceType("Z-Wave Switch")
   }
   [name: "manufacturer", value: cmd.manufacturerName]
@@ -136,23 +167,25 @@ def off() {
 }
 
 def poll() {
+  logDebug "Poll"
   zwave.switchBinaryV1.switchBinaryGet().format()
 }
 
 def refresh() {
   delayBetween([
       zwave.switchBinaryV1.switchBinaryGet().format(),
+      // Causes a ManufacturerSpecificReport to be issued.
       zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
   ])
 }
 
 // Capture preference changes
 def updated() {
-    log.debug "before settings: ${settings.inspect()}, state: ${state.inspect()}"
+    logDebug "current settings: ${settings.inspect()}, state: ${state.inspect()}"
 
     // External switch function settings
     def Short p1 = 0
-    switch (settings.param1) {
+    switch (settings.extSwitchType) {
     case "Disabled":
       p1 = 0
       break
@@ -172,47 +205,50 @@ def updated() {
     
   // Auto-off
   def Short p2 = 0
-  if ("${settings.param2}" == "Never") {
+  if ("${settings.autoShutoffMinutes}" == "Never") {
     p2 = 0
   } else {
-    p2 = (settings.param2 ?: 0).toInteger()
+    p2 = (settings.autoShutoffMinutes ?: 0).toInteger()
+  }
+  
+  def configResponses = []
+  
+  if (p1 != state.extSwitchType) {
+    state.extSwitchType = p1
+    configResponses.add(
+      zwave.configurationV1.configurationSet(
+        parameterNumber: 1, size: 1, configurationValue: [p1]).format())
   }
     
-  if (p1 != state.param1) {
-    state.param1 = p1
-    return response(
-        zwave.configurationV1.configurationSet(
-            parameterNumber: 1, size: 1, configurationValue: [p1]).format())
-  }
-    
-  if (p2 != state.param2) {
-    state.param2 = p2
+  if (p2 != state.autoShutoffMinutes) {
+    state.autoShutoffMinutes = p2
     if (p2 == 0) {
-      return response(
-          delayBetween([
-              zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, configurationValue: [0]).format(),
-              zwave.configurationV1.configurationSet(parameterNumber: 3, size: 1, configurationValue: [0]).format(),
-              zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1, configurationValue: [0]).format()
-          ]))
+      configResponses.addAll([
+        zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, configurationValue: [0]).format(),
+        zwave.configurationV1.configurationSet(parameterNumber: 3, size: 1, configurationValue: [0]).format(),
+        zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1, configurationValue: [0]).format()])
     } else {
-      return response(
-          delayBetween([
-              zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, configurationValue: [p2]).format(),
-              zwave.configurationV1.configurationSet(parameterNumber: 3, size: 1, configurationValue: [232]).format(),
-              zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1, configurationValue: [0]).format()
-          ]))
+      configResponses.addAll([
+        zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, configurationValue: [p2]).format(),
+        zwave.configurationV1.configurationSet(parameterNumber: 3, size: 1, configurationValue: [232]).format(),
+        zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1, configurationValue: [0]).format()])
     }
   }
   
-  log.debug "after settings: ${settings.inspect()}, state: ${state.inspect()}"
+  if (!configResponses.empty) {
+    logDebug "Updating zwave configuration: $configResponses"
+    response(delayBetween(configResponses))
+  }
 }
 
-def configure() {
-  delayBetween([
-      zwave.configurationV1.configurationSet(parameterNumber: 1, size: 1, configurationValue: [3]).format(),
-      zwave.configurationV1.configurationSet(parameterNumber: 2, size: 1, configurationValue: [0]).format(),
-      zwave.configurationV1.configurationSet(parameterNumber: 3, size: 1, configurationValue: [0]).format(),
-      zwave.configurationV1.configurationSet(parameterNumber: 4, size: 1, configurationValue: [0]).format()
-  ])
+/****************************
+ * Logging Functions
+****************************/
+void logInfo(String msg) {
+  if (txtEnable) log.info "${device.displayName}: ${msg}"
+}
+
+void logDebug(String msg) {
+  if (debugEnable) log.debug "${device.displayName}: ${msg}"
 }
 
